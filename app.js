9ebdbb45709b15d8d5cd2e25ab5d1dba72405ce7
Jackson Malone <jrm9918@gmail.com>
2023-03-09 19:11:30 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,28 @@
+function scrollFunction1() {
+  let e = document.getElementById("home");
+  e.scrollIntoView({
+    block: 'start',
+    behavior: 'smooth',
+    inline: 'start'
+  });
+}
+function scrollFunction2() {
+  let e = document.getElementById("projects");
+  // This ends the block to the window 
+  // bottom and also aligns the view to the center 
+  e.scrollIntoView({
+    block: 'end',
+    behavior: 'smooth',
+    inline: 'center'
+  });
+}
+function scrollFunction3() {
+  let e = document.getElementById("contact");
+  // This ends the block to the window 
+  // bottom and also aligns the view to the center 
+  e.scrollIntoView({
+    block: 'end',
+    behavior: 'smooth',
+    inline: 'center'
+  });
+}
